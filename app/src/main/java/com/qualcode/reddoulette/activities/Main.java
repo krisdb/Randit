b0cc77362b953341a0dcc7d4a6775d0ab672e727
krisdb <kris@qualcode.net>
2015-08-18 16:57:50 -0400

    achievments

--- app/src/main/java/com/qualcode/reddoulette/activities/Main.java
+++ app/src/main/java/com/qualcode/reddoulette/activities/Main.java
@@ -12,8 +12,14 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.games.Games;
 import com.qualcode.reddoulette.R;
 import com.qualcode.reddoulette.adapters.PostListRecyclerViewAdapter;
+import com.qualcode.reddoulette.common.BaseGameUtils;
 import com.qualcode.reddoulette.common.DividerItemDecoration;
 import com.qualcode.reddoulette.common.Utilities;
 import com.qualcode.reddoulette.models.RedditPost;
@@ -23,16 +29,30 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main extends AppCompatActivity  {
+public class Main extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener  {
     protected RecyclerView mRecyclerView;
     private List<RedditPost> mPosts = new ArrayList<>();
     SwipeRefreshLayout mSwipeRefreshLayout;
     private String mSubreddit;
+    private GoogleApiClient mGoogleApiClient;
+    private static int RC_SIGN_IN = 9001;
 
+    private boolean mResolvingConnectionFailure = false;
+    private boolean mAutoStartSignInflow = true;
+    private boolean mSignInClicked = false;
+    boolean mExplicitSignOut = false;
+    boolean mInSignInFlow = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Games.API)
+                .addScope(Games.SCOPE_GAMES)
+                .build();
 
         mRecyclerView = (RecyclerView)findViewById(R.id.postlist);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -48,6 +68,88 @@ public class Main extends AppCompatActivity  {
 
         refreshContent();
     }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (mResolvingConnectionFailure) {
+            // already resolving
+            return;
+        }
+
+        // if the sign-in button was clicked or if auto sign-in is enabled,
+        // launch the sign-in flow
+        if (mSignInClicked || mAutoStartSignInflow) {
+            mAutoStartSignInflow = false;
+            mSignInClicked = false;
+            mResolvingConnectionFailure = true;
+
+            // Attempt to resolve the connection failure using BaseGameUtils.
+            // The R.string.signin_other_error value should reference a generic
+            // error string in your strings.xml file, such as "There was
+            // an issue with sign-in, please try again later."
+            if (!BaseGameUtils.resolveConnectionFailure(this, mGoogleApiClient, connectionResult, RC_SIGN_IN, "Error")) {
+                mResolvingConnectionFailure = false;
+            }
+        }
+
+        // Put code here to display the sign-in button
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        // Attempt to reconnect
+        mGoogleApiClient.connect();
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent intent) {
+        if (requestCode == RC_SIGN_IN) {
+            mSignInClicked = false;
+            mResolvingConnectionFailure = false;
+            if (resultCode == RESULT_OK) {
+                mGoogleApiClient.connect();
+            } else {
+                // Bring up an error dialog to alert the user that sign-in
+                // failed. The R.string.signin_failure should reference an error
+                // string in your strings.xml file that tells the user they
+                // could not be signed in, such as "Unable to sign in."
+                BaseGameUtils.showActivityResultError(this, requestCode, resultCode, R.string.signin_failure);
+            }
+        }
+        invalidateOptionsMenu();
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (!mInSignInFlow && !mExplicitSignOut) {
+            // auto sign in
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu (Menu menu) {
+        menu.getItem(2).setVisible(mSignInClicked == false); //sign in
+        menu.getItem(3).setVisible(mSignInClicked); //sign out
+        menu.getItem(4).setVisible(mSignInClicked); //leaderboard
+        menu.getItem(5).setVisible(mSignInClicked); //achievements
+        return true;
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        // The player is signed in. Hide the sign-in button and allow the
+        // player to proceed.
+    }
+
 
     private void refreshContent() {
         mPosts = new ArrayList<>();
@@ -166,6 +268,29 @@ public class Main extends AppCompatActivity  {
 
         if (id == R.id.action_refresh) {
             refreshContent();
+            return true;
+        }
+
+        if (id == R.id.action_signin) {
+            mSignInClicked = true;
+            mGoogleApiClient.connect();
+            return true;
+        }
+
+        if (id == R.id.action_signout) {
+            Games.signOut(mGoogleApiClient);
+            mSignInClicked = false;
+            mGoogleApiClient.disconnect();
+            return true;
+        }
+
+        if (id == R.id.action_leaderboard) {
+            startActivityForResult(Games.Leaderboards.getAllLeaderboardsIntent(mGoogleApiClient), 100);
+            return true;
+        }
+
+        if (id == R.id.action_achievements) {
+            startActivityForResult(Games.Achievements.getAchievementsIntent(mGoogleApiClient), 200);
             return true;
         }
 
